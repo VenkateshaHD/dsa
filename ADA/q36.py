eb8e51d3c501656eeccfbc1c59663447416ad10b
venkatesha <venkyvenkateshahd1999@gmail.com>
2025-07-21 07:56:17 +0530

    q36

---
+++ ADA/q36.py
@@ -0,0 +1,29 @@
+# Search in Rotated Sorted Array
+# Scenario: Find an element in a rotated sorted array.
+# Task: Locate a target using modified binary search.
+# Justification: Exploit sorted structure.
+# Provide proof of Time Complexity: O(log n)
+
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2  
+            if nums[mid] == target:
+                return mid
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+    
+nums = [15, 18,1, 2, 3, 6, 12]
+target = 3
+rotatedArray  = Solution()
+print(rotatedArray.search(nums, target))  # Output: 3
